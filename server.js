1ca23c0c63ff784c6d1a0bc92bcb413505f20347
Owen Hsu <sheauhei@gmail.com>
2022-05-04 17:08:44 +0800

    u

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
-var express = require('express');
-var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
+const { ApolloServer, gql } = require('apollo-server');
+// ApolloServer: 讓我們啟動 server 的 class ，不但實作許多 GraphQL 功能也提供 web application 的功能 (背後使用 express)
+// gql: template literal tag, 讓你在 Javascript 中使用 GraphQL 語法
 
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
+// 1. GraphQL Schema 定義
+const typeDefs = gql`
   type Query {
+    "A simple type for getting started!"
     hello: String
-    world: String
   }
-`);
+`;
 
-// The root provides a resolver function for each API endpoint
-var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-
-  world: () => {
-    return 'Wow!';
-  },
+// 2. Resolvers 是一個會對照 Schema 中 field 的 function map ，讓你可以計算並回傳資料給 GraphQL Server
+const resolvers = {
+  Query: {
+    // 需注意名稱一定要對到 Schema 中 field 的名稱
+    hello: () => 'world'
+  }
 };
 
-var app = express();
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
-app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+// 3. 初始化 Web Server ，需傳入 typeDefs (Schema) 與 resolvers (Resolver)
+const server = new ApolloServer({
+  // Schema 部分
+  typeDefs,
+  // Resolver 部分
+  resolvers
+});
+
+// 4. 啟動 Server
+server.listen().then(({ url }) => {
+  console.log(`? Server ready at ${url}`);
+});
+
+// app.listen(4000);
+// console.log('Running a GraphQL API server at http://localhost:4000/graphql');
